5e21a30e39ba43263b3135cbf3a36a43f1fe3ba1
danixx j p <daninafria@hotmail.es>
2019-09-30 00:15:01 +0200

    WIP server estructure

--- Servidor/server/src/main/java/server/Room.java
+++ Servidor/server/src/main/java/server/Room.java
@@ -1,13 +1,47 @@
 package server;
 
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.springframework.web.socket.WebSocketSession;
 
 public class Room {
 	String nombre;
-	public final int maxNumPlayers = 4;
-	ConcurrentHashMap<WebSocketSession,JugadorConectado> jugadoresEnSala = new ConcurrentHashMap<WebSocketSession, JugadorConectado>();
+	public final int MAXNUMPLAYERS = 4;
+	int numPlayers = 0;
+	boolean hasStart = false;
+	ReentrantLock lock = new ReentrantLock();
+	HashMap<WebSocketSession,JugadorConectado> jugadoresEnSala = new HashMap<WebSocketSession, JugadorConectado>();
+	
+	
+	public Room( String nombre,JugadorConectado jug) {
+		this.nombre = nombre;
+		lock.lock();
+		jugadoresEnSala.putIfAbsent(jug.getSession(), jug);
+		numPlayers++;
+		lock.unlock();
+		
+		
+	}
+	
+	public void anadirJugador(JugadorConectado jug) {
+		lock.lock();
+		if(jugadoresEnSala.putIfAbsent(jug.getSession(), jug)!= null) {
+			numPlayers++;
+		};
+		if(numPlayers == MAXNUMPLAYERS) {
+			hasStart = true;
+		}
+		lock.unlock();
+	}
+	
+	public void quitarJugador(JugadorConectado jug) {
+		lock.lock();
+		if(jugadoresEnSala.remove(jug.getSession())!= null) {
+			numPlayers--;
+		};
+		lock.unlock();
+	}
 	
 }
